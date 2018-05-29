8a17a86616857530221870806c28843bf0cc81aa
Shindo S <lanethcs@hotmail.com>
2018-05-29 14:22:23 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
-position_taken?(board, index)
+def position_taken?(board, index)
   position = board[index]
   if position == " " || position == "" || position == nil
-    false
+    return false
+  end
+  true
 end
